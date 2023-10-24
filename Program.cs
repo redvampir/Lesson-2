66edc16f7b0b1767cf838e907eb20cf6518f0719
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-24 19:25:35 +0300

    Урок 48 Виртуальные методы

--- Program.cs
+++ Program.cs
@@ -11,87 +11,50 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            Fighter[] fighters =
+            NonPlayerCharacter[] characters =
             {
-                 new Fighter("Джон", 500, 50, 0),
-                 new Fighter("Марк", 250, 25, 20),
-                 new Fighter("Алекс", 150, 100, 10),
-                 new Fighter("Джон", 300, 75, 5)
+                new NonPlayerCharacter(),
+                new Farmer(),
+                new Knight(),
+                new Child()
             };
 
-            int fighterNumber;
-
-            for (int i = 0; i < fighters.Length; i++)
+            foreach (var character in characters)
             {
-                Console.Write((i + 1) + " ");
-                fighters[i].ShowStats();
+                character.ShowDescription();
+                Console.WriteLine(new string('-', 40));
             }
-
-            Console.WriteLine("\n** "+ new string('-', 25) + " **");
-            Console.Write("\nВыберите номер первого бойца: ");
-            fighterNumber = Convert.ToInt32(Console.ReadLine()) - 1;
-            Fighter firstFighter = fighters[fighterNumber];
-
-            Console.Write("\nВыберите номер второго бойца: ");
-            fighterNumber = Convert.ToInt32(Console.ReadLine()) - 1;
-            Fighter secondFighter = fighters[fighterNumber];
-            Console.WriteLine("\n** " + new string('-', 25) + " **");
-
-            while (firstFighter.Health >0 && secondFighter.Health > 0)
-            {
-                firstFighter.TakeDamage(secondFighter.Damage);
-                secondFighter.TakeDamage(firstFighter.Damage);
-                firstFighter.ShowCurrentHealth();
-                secondFighter.ShowCurrentHealth();
-            }
-        }   
-
-    }
-
-    class Fighter
-    {
-        private string _name;
-        private int _health;
-        private int _damage;
-        private int _armor;
-    public Fighter (string name, int health, int damage, int armor)
-        {
-            _name = name;
-            _health = health;
-            _damage = damage;
-            _armor = armor;
         }
 
-    public int Health
-    {
-       get 
-       { 
-          return _health;
-       }
     }
 
-    public int Damage
+    class NonPlayerCharacter
     { 
-        get
-       { 
-            return _damage; 
-       }
-    }
-    public void ShowStats()
+        public virtual void ShowDescription()
         {
-            Console.WriteLine($"Боец - {_name},здоровье: {_health}, наносимый урон:{_damage}, броня{_armor}");
-        }
-
-        public void ShowCurrentHealth()
-        {
-            Console.WriteLine($"{_name} здоровье: {_health}");
-        }
-
-        public void TakeDamage( int damage )
-        {
-            _health -= damage - _armor;
+            Console.WriteLine("Я простой NPC, умею только гулять.");
         }
     }
 
+    class Farmer : NonPlayerCharacter
+    {
+        public override void ShowDescription()
+        {
+            base.ShowDescription();
+            Console.WriteLine("А ещё я фермер и умею работать на поле.");
+        }
+    }
 
+    class Knight : NonPlayerCharacter
+    {
+        public override void ShowDescription()
+        {
+            Console.WriteLine("Я рыцарь, мое дело только сражения!"); ;
+        }
+    }
+
+    class Child : NonPlayerCharacter
+    {
+
+    }
 }
