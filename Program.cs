cc9391e22b6d5b864fd959f8f5335f6692de8a08
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-24 13:09:43 +0300

    Урок 47 Практика ООП Бой героев

--- Program.cs
+++ Program.cs
@@ -11,38 +11,87 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-           Renderer renderer = new Renderer();
-            Player player = new Player(55,10);
+            Fighter[] fighters =
+            {
+                 new Fighter("Джон", 500, 50, 0),
+                 new Fighter("Марк", 250, 25, 20),
+                 new Fighter("Алекс", 150, 100, 10),
+                 new Fighter("Джон", 300, 75, 5)
+            };
 
-            renderer.Draw(player.X,player.Y);
-        }
+            int fighterNumber;
+
+            for (int i = 0; i < fighters.Length; i++)
+            {
+                Console.Write((i + 1) + " ");
+                fighters[i].ShowStats();
+            }
+
+            Console.WriteLine("\n** "+ new string('-', 25) + " **");
+            Console.Write("\nВыберите номер первого бойца: ");
+            fighterNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+            Fighter firstFighter = fighters[fighterNumber];
+
+            Console.Write("\nВыберите номер второго бойца: ");
+            fighterNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+            Fighter secondFighter = fighters[fighterNumber];
+            Console.WriteLine("\n** " + new string('-', 25) + " **");
+
+            while (firstFighter.Health >0 && secondFighter.Health > 0)
+            {
+                firstFighter.TakeDamage(secondFighter.Damage);
+                secondFighter.TakeDamage(firstFighter.Damage);
+                firstFighter.ShowCurrentHealth();
+                secondFighter.ShowCurrentHealth();
+            }
+        }   
+
     }
 
-    class Renderer
+    class Fighter
     {
-        public void Draw(int x, int y, char character = '@')
+        private string _name;
+        private int _health;
+        private int _damage;
+        private int _armor;
+    public Fighter (string name, int health, int damage, int armor)
         {
-            Console.CursorVisible = false;
-            Console.SetCursorPosition(x, y);
-            Console.Write(character);
-            Console.ReadKey(true);
+            _name = name;
+            _health = health;
+            _damage = damage;
+            _armor = armor;
         }
-    }
 
-   class Player
+    public int Health
     {
-        private int _x;
-        private int _y;
+       get 
+       { 
+          return _health;
+       }
+    }
 
-        public int X { get; private set; }
-
-        public int Y { get; private set; }
-
-        public Player(int x, int y)
+    public int Damage
+    { 
+        get
+       { 
+            return _damage; 
+       }
+    }
+    public void ShowStats()
         {
-            X = x;
-            Y = y;
+            Console.WriteLine($"Боец - {_name},здоровье: {_health}, наносимый урон:{_damage}, броня{_armor}");
+        }
+
+        public void ShowCurrentHealth()
+        {
+            Console.WriteLine($"{_name} здоровье: {_health}");
+        }
+
+        public void TakeDamage( int damage )
+        {
+            _health -= damage - _armor;
         }
     }
+
 
 }
