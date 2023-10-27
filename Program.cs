7e7d96f9872d64f1e80b734ce804f1051a5e1b69
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-27 15:03:37 +0300

    Урок 55 Практика ООП Симулятор компьютерного клуба.

--- Program.cs
+++ Program.cs
@@ -11,27 +11,190 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            Vector2 position = new Vector2(10);
-
-            Console.WriteLine(position.Y);
+           CumputerClub cumputerClub = new CumputerClub (8);
+            cumputerClub.Work();
         }
        
     }
-    
-    struct Vector2
+    class CumputerClub
     {
-        public int X, Y;
+        private int _money = 0;
+        private List<Computer> _computers = new List<Computer>();
+        private Queue<Client> _clients = new Queue<Client>();
 
-        public Vector2(int x, int y)
+        public CumputerClub(int coumputersCount) 
         {
-            X = x;
-            Y = y;
+            Random random = new Random();
+
+            for (int i = 0; i < coumputersCount; i++)
+            {
+                _computers.Add(new Computer(random.Next(5, 15)));
+            }
+
+            CreateNewClients(25, random);
         }
 
-        public Vector2(int x) : this() 
+        public void CreateNewClients (int count, Random random)
+        {
+            for (int i = 0;i < count;i++)
+            {
+                _clients.Enqueue(new Client(random.Next(100, 250), random));
+            }
+        }
+
+        public void Work()
+        {
+            while (_clients.Count > 0)
+            {
+                Client newClient = _clients.Dequeue();
+                Console.WriteLine($"Баланс компьютерного клуба {_money} руб. Ждем нового клиента.");
+                Console.WriteLine($"У вас новый клиент, и он хочет купить {newClient.DesiredMinutes} минут.");
+                ShowAllComputesState();
+
+                Console.Write("\nВы предлагаете ему компьютер под номером: ");
+                string userInput = Console.ReadLine();
+
+                if(int.TryParse(userInput, out int computerNumper))
+                {
+                    computerNumper -= 1;
+
+                    if(computerNumper >= 0 && computerNumper < _computers.Count)
+                    {
+                        if (_computers[computerNumper].IsTaken)
+                        {
+                            Console.WriteLine("Вы пытаетесь посадить клиента, за компьютер, который уже занят. Клиент разозлился и ушел");
+                        }
+                        else
+                        {
+                            if (newClient.CheckSolvency(_computers[computerNumper]))
+                            {
+                                Console.WriteLine("Клиент пересчитал деньги, оплатил и сел за компьютер " + (computerNumper + 1));
+                                _money += newClient.Pay();
+                                _computers[computerNumper].BecomeTaken(newClient);
+                            }
+                            else
+                            {
+                                Console.WriteLine("У клиента не хватило денег и он ушёл.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы сами не знаете за какой компьютер посадить клиента. Он разозлися и ушёл.");
+                    }
+                }
+                else
+                {
+                    CreateNewClients(1, new Random());
+                    Console.WriteLine("Неверный ввод! Повторите снова.");
+                }
+
+                Console.WriteLine("Чтобы перейти к следующему клиенту, нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+                SpendOneMinute();
+            }
+        }
+
+        private void ShowAllComputesState ()
+        {
+            Console.WriteLine("\nСписок всех компьютеров:");
+            for (int i = 0; i < _computers.Count; i++)
+            {
+                Console.Write(i + 1 + " - ");
+                _computers[i].ShowState();
+            }
+        }
+
+        private void SpendOneMinute ()
+        {
+            foreach (var computer in _computers)
+            {
+                computer.SpendOneMinute();
+            }
+        }
+    }
+
+    class Computer
+    {
+        private Client _client;
+        private int _minutesRemaining;
+        public bool IsTaken
         { 
-            X = x; 
+            get
+            {
+                return _minutesRemaining > 0;
+            }
         }
 
+        public int PricePerMinute { get; private set; }
+
+        public Computer(int pricePerMinute)
+        {
+            PricePerMinute = pricePerMinute;
+        }
+
+        public void BecomeTaken (Client client)
+        {
+            _client = client;
+            _minutesRemaining = _client.DesiredMinutes;
+        }
+
+        public void BecomeEmpty()
+        {
+            _client = null;
+        }
+
+        public void SpendOneMinute()
+        {
+            _minutesRemaining--;
+        }
+
+        public void ShowState()
+        {
+            if(IsTaken)
+            {
+                Console.WriteLine($"Компьютер занят, осталось минут: {_minutesRemaining}");
+            }
+            else
+            {
+                Console.WriteLine($"Компьютер свободен, цена за минуту: {PricePerMinute}");
+            }
+
+        }
+    }
+
+    class Client
+    {
+        private int _money;
+        private int _moneyToPay;
+
+        public int DesiredMinutes { get; private set; }
+
+        public Client(int money, Random random) 
+        {
+            _money = money;
+            DesiredMinutes = random.Next(10, 30);
+        }
+
+        public bool CheckSolvency(Computer computer)
+        {
+            _moneyToPay = DesiredMinutes * computer.PricePerMinute;
+            if ( _money >= _moneyToPay )
+            {
+                return true;
+            }
+            else 
+            {
+                _moneyToPay = 0;
+                return false; 
+            }
+        }
+
+        public int Pay()
+        {
+            _money -= _moneyToPay;
+            return _moneyToPay;
+        }
     }
 }
